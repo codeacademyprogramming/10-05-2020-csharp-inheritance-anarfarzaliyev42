f47dda88d9b814f17fdb57cbf8f75b75dd6d8c23
anarfarzaliyev9 <anarfarzaliyev9@gmail.com>
2020-05-10 20:24:04 +0400

    add

---
+++ Task/Task/ProDocumentProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class ProDocumentProgram : DocumentProgram
+    {
+        sealed public override void EditDocument()
+        {
+            Console.WriteLine("Document Edited");
+        }
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Document Saved in doc format, for pdf format buy Expert packet");
+        }
+
+    }
+}
---
+++ Task/Task/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetInputs();
+
+            Console.ReadKey();
+        }
+        static void GetInputs()
+        {
+            string keyWord = Console.ReadLine();
+
+            CheckKeyword(keyWord);
+        }
+        static void CheckKeyword(string keyWord)
+        {
+            DocumentProgram documentProgram;
+            switch (keyWord)
+            {
+                case "basic":
+                    documentProgram = new DocumentProgram();
+                    break;
+                case "pro":
+                    documentProgram = new ProDocumentProgram();
+                    break;
+                case "expert":
+                    documentProgram = new ExpertDocument();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Keyword");
+                    return;
+
+            }
+            ShowOperations(documentProgram);
+
+        }
+        static void ShowOperations(DocumentProgram documentProgram)
+        {
+            documentProgram.OpenDocument();
+            documentProgram.EditDocument();
+            documentProgram.SaveDocument();
+        }
+    }
+
+}
